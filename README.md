23c4e3e1132bf6def1c0bbea4c7356377d3ba32f
lamphancs <121737038+lamphancs@users.noreply.github.com>
2023-02-28 23:46:58 -0800

    topbar set up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pharmaVendia
  
+# Create app
+npx create-react-app pharmastudy
+
+# Install toolkits
+- Move to directory.
+- npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar formik yup @fullcalendar/core @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo
---
+++ pharmastudy/src/App.js
@@ -0,0 +1,24 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar";
+import { useState } from "react";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
