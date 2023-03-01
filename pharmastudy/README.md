fbb3c06e693a828fab995690d3c8e6a77dfef361
lamphancs <121737038+lamphancs@users.noreply.github.com>
2023-03-01 01:23:16 -0800

    side bar set up

--- pharmastudy/README.md
+++ pharmastudy/README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+###### ###### #######
+## SET UP
+# Create app
+npx create-react-app pharmastudy
+
+# Install toolkits
+- Move to directory.
+- npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar@0.7.1 formik yup @fullcalendar/core @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo
--- pharmastudy/src/App.js
+++ pharmastudy/src/App.js
@@ -1,7 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Topbar from "./scenes/global/Topbar";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,9 +16,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
           <div className="app">
-            <main className="content">
-              <Topbar setIsSidebar={setIsSidebar} />
-            </main>
+            <SideBar isSidebar={isSidebar} />
+              <main className="content">
+                <TopBar setIsSidebar={setIsSidebar} />
+                <Routes>
+                  {/* <Route path="/" element={<Dashboard />} /> */}
+                  {/* <Route path="/team" element={<Team />} /> */}
+                  {/* <Route path="/contacts" element={<Contacts />} /> */}
+                  {/* <Route path="/invoices" element={<Invoices />} /> */}
+                  {/* <Route path="/form" element={<Form />} /> */}
+                  {/* <Route path="/bar" element={<Bar />} /> */}
+                  {/* <Route path="/pie" element={<Pie />} /> */}
+                  {/* <Route path="/line" element={<Line />} /> */}
+                  {/* <Route path="/faq" element={<FAQ />} /> */}
+                  {/* <Route path="/calendar" element={<Calendar />} /> */}
+                  {/* <Route path="/geography" element={<Geography />} /> */}
+                </Routes>
+              </main>
           </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
